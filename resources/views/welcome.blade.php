3a15e1f9466f9f6fa6ca33dffdf2024cefe22008
DanielCoyle <dancoyle89@gmail.com>
2019-09-25 12:30:36 -0400

    Login Routing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/clients') }}">Clients</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
